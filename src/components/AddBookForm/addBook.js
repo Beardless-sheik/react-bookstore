578366cc6a000fa51efdb9fac49d33cc8fa2aae0
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-16 00:25:30 +0200

    Create AddBook Form component

---
+++ src/components/AddBookForm/addBook.js
@@ -0,0 +1,25 @@
+import styles from './addBook.module.css';
+
+const AddBookForm = () => {
+  return(
+    <>
+      <div className={styles.bookFormContainer}>
+      <div className={styles.addBookHeadingText}> ADD NEW BOOK </div>
+        <form>
+          <input className={styles.inputBookTitle} placeholder='Book Title' required/> 
+          <select className={styles.inputCategoryBook} name="Category" id="bookCategory" required >
+            <option value="Action">Action</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Economy">Economy</option>
+            <option value="Biography">Biography</option>
+          </select>
+          <button type="submit" className={styles.addBookSubmitbutton}> 
+            <span className={styles.submitButtonText}> ADD BOOK </span>
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default AddBookForm;
