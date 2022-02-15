7e87963975729be913aea46b28aa7399fe5b6a2d
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-16 01:48:31 +0200

    Implement dummy data for books to render first UI

--- src/components/BookListContainer/books.js
+++ src/components/BookListContainer/books.js
@@ -2,9 +2,33 @@ import styles from './books.module.css';
 import BookItem from '../BookItem/bookItem';
 
 const BookListContainer = (props) => {
-  const numbers = [{id:1},{id:2},{id:3}]
-  const bookItems = numbers.map((element) => 
-    <BookItem key={element.id}/>
+  const books = [{
+    id:1,
+    genre: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    percentageCompleted: '64%',
+    currentChapter: 'Chapter 7',
+  },
+  {
+    id:2,
+    genre: 'Science Fiction',
+    title: 'Dune',
+    author: 'Science Fiction',
+    percentageCompleted: '8%',
+    currentChapter: 'Chapter 3: "A Lesson Learned"'
+  },
+  {
+    id:3,
+    genre: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    percentageCompleted: '16%',
+    currentChapter: 'Introduction',
+  }]
+  const bookItems = books.map((element) => {
+    console.log(element);
+    return <BookItem key={element.id} {...element} />}
   );
 
   return(
