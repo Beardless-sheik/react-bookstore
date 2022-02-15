ec5eaa3e74c1d753400209933b1617ead5488706
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-16 01:48:00 +0200

    Add default props in case of lack of incoming data

--- src/components/BookItem/bookItem.js
+++ src/components/BookItem/bookItem.js
@@ -1,13 +1,14 @@
 import styles from './bookItem.module.css';
 
 const BookItem = (props) => {
+  console.log(props.author)
   return(
     <>
       <div className={styles.bookListContainer}>
         <div className={styles.bookDetailsContainer}>
-          <div className={styles.bookGenreText}>{props.genre} </div>
-          <div className={styles.bookTitleText}> {props.title} </div>
-          <div className={styles.bookAuthor}> {props.author} </div>
+          <div className={styles.bookGenreText}>{props.genre || 'Genre Not Given' } </div>
+          <div className={styles.bookTitleText}> {props.title || 'Title Not Given' } </div>
+          <div className={styles.bookAuthor}> {props.author || 'Author Not Given'} </div>
           <div className={styles.bookMenuButtonContainer} >
             <button className={styles.bookMenuButton}> Comments </button>
             <span className={styles.Line}></span> 
@@ -25,14 +26,14 @@ const BookItem = (props) => {
             </div>
           </div>
           <div className={styles.bookCompletionValueContainer}>
-            <div className={styles.bookCompletionTextValue}> {props.percentageCompleted} </div>
+            <div className={styles.bookCompletionTextValue}> {props.percentageCompleted || '% unavailable'} </div>
             <div className={styles.bookCompletionBoolean}> Completed </div>
           </div>
           <span className={styles.Line2}></span> 
         </div>
         <div className={styles.bookCurrentChapterContainer}>
           <div className={styles.currentChapterHeadingText}> CURRENT CHAPTER </div>
-          <div className={styles.currentChapterNumberHeadingText}> {props.currentChapter}  </div>
+          <div className={styles.currentChapterNumberHeadingText}> {props.currentChapter || 'Current Chapter Unavailable'}  </div>
           <button className={styles.updateButton}> 
             <span className={styles.updateButtonText}>UPDATE PROGRESS </span> 
           </button>
