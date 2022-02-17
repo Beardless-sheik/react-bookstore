5d4d152ec30e2ace1331c43db747ffa8eb81408f
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-17 11:12:10 +0200

    Implement Adding of a Book functionality
    - Add two controlled input variables from the form to be added to the book.
    - Implement 2 event listeners to handle these changes given UI user input.
    - Implement a submit book event listener function for when user clicks on the UI submit button

--- src/components/AddBookForm/addBook.js
+++ src/components/AddBookForm/addBook.js
@@ -1,23 +1,51 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 import styles from './addBook.module.css';
 
-const AddBookForm = () => (
-  <>
-    <div className={styles.bookFormContainer}>
-      <div className={styles.addBookHeadingText}> ADD NEW BOOK </div>
-      <form>
-        <input className={styles.inputBookTitle} placeholder="Book Title" required />
-        <select className={styles.inputCategoryBook} name="Category" id="bookCategory" required>
-          <option value="Action">Action</option>
-          <option value="Science Fiction">Science Fiction</option>
-          <option value="Economy">Economy</option>
-          <option value="Biography">Biography</option>
-        </select>
-        <button type="submit" className={styles.addBookSubmitbutton}>
-          <span className={styles.submitButtonText}> ADD BOOK </span>
-        </button>
-      </form>
-    </div>
-  </>
-);
+const AddBookForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState('');
+
+  const handleBookTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleGenreTitleChange = (event) => {
+    setGenre(event.target.value);
+  };
+
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      genre,
+      author: 'author',
+    };
+    dispatch(addBook(newBook));
+  };
+  return (
+    <>
+      <div className={styles.bookFormContainer}>
+        <div className={styles.addBookHeadingText}> ADD NEW BOOK </div>
+        <form>
+          <input className={styles.inputBookTitle} value={title} placeholder="Book Title" onChange={handleBookTitleChange} required />
+          <select className={styles.inputCategoryBook} onChange={handleGenreTitleChange} name="Category" id="bookCategory" required>
+            <option value="Action">Action</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Economy">Economy</option>
+            <option value="Biography">Biography</option>
+          </select>
+          <button type="submit" onClick={submitBookToStore} className={styles.addBookSubmitbutton}>
+            <span className={styles.submitButtonText}> ADD BOOK </span>
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default AddBookForm;
