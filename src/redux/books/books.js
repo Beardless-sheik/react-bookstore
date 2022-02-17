486a5ec442f4677b08df7cb7015abc651dd9ef69
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-17 21:23:44 +0200

    Add thunks to connect to API and react with redux store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,7 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const GET_BOOKS = 'bookStore/books/GET_BOOKS';
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rVyokk2snM83teww3zCy/books';
 const initialState = [];
 
 export const addBook = (payload) => ({
@@ -24,7 +25,7 @@ const booksReducer = (state = initialState, action) => {
       return [...state, action.payload];
 
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);
+      return state.filter((book) => book.id !== action.payload);
 
     case GET_BOOKS:
       return action.payload;
@@ -32,6 +33,54 @@ const booksReducer = (state = initialState, action) => {
     default:
       return state;
   }
+};
+
+export const getBooksListAPIThunk = () => async (dispatch) => {
+  let bookList = [];
+  try {
+    const result = await fetch(baseURL);
+    bookList = await result.json();
+  } catch (error) {
+    bookList = [{
+      error,
+    }];
+  }
+  const booksID = Object.keys(bookList);
+  const formatedBooks = [];
+  booksID.map((key) => formatedBooks.push({
+    id: key,
+    title: bookList[key][0].title,
+    genre: bookList[key][0].category,
+  }));
+  dispatch(getBooks(formatedBooks));
+};
+
+export const postBookAPIThunk = (newBook) => async (dispatch) => {
+  await fetch(baseURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: newBook.id,
+      title: newBook.title,
+      category: newBook.category,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(addBook(newBook));
+};
+
+export const deleteBookAPIThunk = (id) => async (dispatch) => {
+  await fetch(`${baseURL}/${id}`, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(removeBook(id));
 };
 
 export default booksReducer;
